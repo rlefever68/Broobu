e3eb88b79d32de58177b34868f0e5cd4f7a413b1
Rafael Lefever <rafael.lefever@gmail.com>
2014-09-14 22:40:55 +0200

    added monitordisco Test

---
+++ Release/Contracts/Test/Broobu.MonitorDisco.Contract.Test/MonitorDiscoTestFixture.cs
@@ -0,0 +1,84 @@
+﻿// ***********************************************************************
+// Assembly         : Broobu.MonitorDisco.Contract.Test
+// Author           : ON8RL
+// Created          : 12-04-2013
+//
+// Last Modified By : ON8RL
+// Last Modified On : 12-04-2013
+// ***********************************************************************
+// <copyright file="MonitorDiscoTestFixture.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+using Broobu.MonitorDisco.Contract.Agent;
+using Broobu.MonitorDisco.Contract.Domain;
+using Broobu.MonitorDisco.Contract.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wulka.Agent;
+using Wulka.Domain;
+
+namespace Broobu.MonitorDisco.Contract.Test
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class MonitorDiscoTestFixture : IMonitorDisco
+    {
+
+        /// <summary>
+        /// Tries the get all endpoints.
+        /// </summary>
+        [TestMethod]
+        public void Try_GetAllEndpoints()
+        {
+            var res = GetAllEndpoints();
+            Console.WriteLine("Contract\tEndpoint");
+            Console.WriteLine("----------------------------");
+            foreach (var discoInfo in res)
+            {
+                Console.WriteLine("{0}\t{1}",discoInfo.Contract, discoInfo.Endpoint);
+            }
+            Assert.IsNotNull(res);
+        }
+
+
+        [TestMethod]
+        public void Try_GetMonitorDiscoEndpoints()
+        {
+            var res = GetEndpoints(String.Format("{0}:{1}", MonitorDiscoServiceConst.Namespace, "IMonitorDisco"));
+            foreach (var serializableEndpoint in res)
+            {
+                Console.WriteLine(String.Format("Contract: {0}", serializableEndpoint.ContractName));
+            }
+        }
+
+        /// <summary>
+        /// Gets the endpoints.
+        /// </summary>
+        /// <param name="contractType">Type of the contract.</param>
+        /// <returns>SerializableEndpoint[][].</returns>
+        public SerializableEndpoint[] GetEndpoints(string contractType)
+        {
+            return DiscoPortal
+                .Disco
+                .GetEndpoints(contractType);
+        }
+
+
+
+        /// <summary>
+        /// Gets all endpoints.
+        /// </summary>
+        /// <returns>DiscoViewItem[][].</returns>
+        public DiscoInfo[] GetAllEndpoints()
+        {
+            return MonitorDiscoPortal
+                .Agent
+                .GetAllEndpoints();
+        }
+    }
+}
---
+++ Release/Contracts/Test/Broobu.MonitorDisco.Contract.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Broobu.MonitorDisco.Contract.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("603ef916-06a2-4fb6-912c-e79e66772503")]
+
+
